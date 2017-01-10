ea7db16df22bcaa51160cd2389e94e3d0305a3b4
daiski21 <ampitmelving@gmail.com>
2017-01-10 12:01:53 +0800

    Update contactus.js

--- routes/contactus.js
+++ routes/contactus.js
@@ -11,14 +11,14 @@ router.post('/send', function(req, res, next){
 	var trasporter = nodemailer.createTransport({
 		service: 'Gmail',
 		auth: {
-			user: 'ampitmelving@gmail.com',
-			pass: 'Nh66ter15',
+			user: 'ilounge.co@gmail.com',
+			pass: 'password12345',
 		}
 	});
 
 	var mailOptions = {
 		from: 'Name <name@outlook.com>',
-		to: 'ampitmelving@gmail.com',
+		to: 'ilounge.co@gmail.com',
 		subject: 'inquiries',
 		text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ 'Items: '+req.body.item+ 'Message: '+req.body.Message,
 		html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Items: '+req.body.item+ '</li><li>Message: '+req.body.message+ '</li></ul>'
