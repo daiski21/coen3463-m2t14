f262ccceb52eae648215b4dd746875ac02cb2afe
daiski21 <ampitmelving@gmail.com>
2017-01-17 19:44:22 +0800

    Update contactus.js

--- routes/contactus.js
+++ routes/contactus.js
@@ -18,7 +18,7 @@ router.post('/send', function(req, res, next){
 
 	var mailOptions = {
 		from: 'Name <name@outlook.com>',
-		to: 'ampitmelvin@gmail.com',
+		to: 'ampitmelving@gmail.com',
 		subject: 'inquiries',
 		text: 'you have a new message... Name: '+req.body.name+ ' Email: '+req.body.email+ 'Items: '+req.body.item+ 'Message: '+req.body.Message,
 		html: '<p>You got a new message with the following details..</p><ul><li>Name: '+req.body.name+ '</li><li>Email: '+req.body.email+ '</li><li>Items: '+req.body.item+ '</li><li>Message: '+req.body.message+ '</li></ul>'
